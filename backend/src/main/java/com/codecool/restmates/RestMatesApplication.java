f76c06584ce09c8cf5702fb8dd81342261867a89
szidzse <gellertsos@gmail.com>
2024-07-22 13:40:20 +0200

    initialize backend and frontend folder and it's projects

---
+++ backend/src/main/java/com/codecool/restmates/RestMatesApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.restmates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestMatesApplication.class, args);
+	}
+
+}
