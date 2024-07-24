b51928cdf64040f28f7ee502a251a81372fbf611
szidzse <gellertsos@gmail.com>
2024-07-24 12:41:14 +0200

    refactor DatabaseConnection, delete configuration package

--- backend/src/main/java/com/codecool/restmates/database/DatabaseConnection.java
+++ backend/src/main/java/com/codecool/restmates/database/DatabaseConnection.java
@@ -1,7 +1,9 @@
 package com.codecool.restmates.database;
 
-import com.codecool.restmates.configuration.Configuration;
+import com.codecool.restmates.service.member.DAO.MemberDAO;
+import com.codecool.restmates.service.member.DAO.MemberDAOImpl;
 import org.postgresql.ds.PGSimpleDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
 import javax.sql.DataSource;
@@ -9,23 +11,48 @@ import java.sql.SQLException;
 
 @org.springframework.context.annotation.Configuration
 public class DatabaseConnection {
-    private Configuration configuration;
 
-    public DatabaseConnection(Configuration configuration) {
-        this.configuration = configuration;
+    @Value("${restmates.database.url}")
+    private String databaseUrl;
+    @Value("${restmates.database.username}")
+    private String databaseUsername;
+    @Value("${restmates.database.password}")
+    private String databasePassword;
+
+    public DatabaseConnection() {
+
     }
+
+    public String getDatabaseUsername() {
+        return databaseUsername;
+    }
+
+    public String getDatabasePassword() {
+        return databasePassword;
+    }
+
+    public String getDatabaseName() {
+        String[] s = databaseUrl.split("/");
+        return s[s.length - 1];
+    }
+
 
     @Bean
     public DataSource getDataSource() throws SQLException {
         PGSimpleDataSource dataSource = new PGSimpleDataSource();
-        dataSource.setDatabaseName(configuration.getDatabaseName());
-        dataSource.setUser(configuration.getDatabaseUsername());
-        dataSource.setPassword(configuration.getDatabasePassword());
+        dataSource.setDatabaseName(getDatabaseName());
+        dataSource.setUser(getDatabaseUsername());
+        dataSource.setPassword(getDatabasePassword());
 
         System.out.println("Trying to connect...");
         dataSource.getConnection().close();
         System.out.println("Connection closed successfully.");
 
         return dataSource;
+    }
+
+    @Bean
+    public MemberDAO memberDAO() throws SQLException {
+        return new MemberDAOImpl(getDataSource());
     }
 }
