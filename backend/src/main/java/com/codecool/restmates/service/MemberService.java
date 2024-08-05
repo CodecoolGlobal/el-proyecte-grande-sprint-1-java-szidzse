71fffe9c7392d7fe3838133eac886f434c8b79da
Vazul15 <145293185+Vazul15@users.noreply.github.com>
2024-08-05 14:45:25 +0200

    Implemented member repository and member service, save and findByEmail method

--- backend/src/main/java/com/codecool/restmates/service/MemberService.java
+++ backend/src/main/java/com/codecool/restmates/service/MemberService.java
@@ -1,8 +1,28 @@
 package com.codecool.restmates.service;
 
+import com.codecool.restmates.exception.EmailAlreadyExistsException;
+import com.codecool.restmates.model.Member;
+import com.codecool.restmates.repository.MemberRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class MemberService {
+    private final MemberRepository memberRepository;
 
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Optional<Member> findMemberByEmail(long Id) {
+        return memberRepository.findById(Id);
+    }
+
+    public Member saveMember(Member member) {
+        if (memberRepository.existsByEmail(member.getEmail())) {
+            throw new EmailAlreadyExistsException(String.format(" %s already exists.", member.getEmail()));
+        }
+        return memberRepository.save(member);
+    }
 }
