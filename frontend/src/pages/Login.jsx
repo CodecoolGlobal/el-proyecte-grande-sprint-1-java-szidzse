0b311f33af5ae1d1de96d2fe6e53791bc0030126
Vazul15 <145293185+Vazul15@users.noreply.github.com>
2024-08-07 19:49:02 +0200

    Implemented contextProvider and member can Login successfully

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,24 +1,45 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import LoginForm from '../components/LoginForm';
+import { useAuth } from "../components/AuthProvider";
 
-// TODO Under Implemention!! //
+const loginMember = async (memberEmailPassword) => {
+    try {
+        const response = await fetch('/api/member/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(memberEmailPassword),
+        });
 
-// const loginMember = async(memberEmailPassword) => {
-//     const res = await fetch('/api/member')
+        const result = await response.json();
+        return result;
 
-// }
+    } catch (error) {
+        console.error('Error:', error);
+        return false;
+    }
+};
 
 export const Login = () => {
     const navigate = useNavigate();
+    const { login } = useAuth();
 
     const handleSignUp = () => {
         navigate('/signup')
     }
 
-    const handleLogin = (memberEmailPassword) => {
-        navigate('/')
-    }
+    const handleLogin = async (memberEmailPassword) => {
+        const success = await loginMember(memberEmailPassword);
+        if (success) {
+          login();
+          navigate('/');
+          console.log('Login successful');
+        } else {
+          console.log('Login failed');
+        }
+      };
 
   return (
     <LoginForm
