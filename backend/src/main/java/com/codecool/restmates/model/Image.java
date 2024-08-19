46da71e09ea9687e4bf864e524257cafad22e578
szidzse <gellertsos@gmail.com>
2024-08-19 16:31:36 +0200

    create image entity

---
+++ backend/src/main/java/com/codecool/restmates/model/Image.java
@@ -0,0 +1,27 @@
+package com.codecool.restmates.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "image")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String type;
+
+    @Lob
+    @Column(name = "image_data", length = 1000)
+    private byte[] imageData;
+}
