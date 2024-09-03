363d92331e75241d050b4f021b930c4c9be0f692
Vazul15 <145293185+Vazul15@users.noreply.github.com>
2024-09-03 13:05:11 +0200

    user crud implemented

--- frontend/src/components/navigationBar/Footer.jsx
+++ frontend/src/components/navigationBar/Footer.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 
 const Footer = () => (
-  <footer className="footer footer-center bg-base-100 text-base-content p-4 md:fixed md:bottom-0 md:w-full shadow-inner ">
-    <aside>
-      <p>
-        © RESTMates {new Date().getFullYear()} - All rights reserved by
-        RESTMates Industries Ltd
-      </p>
-    </aside>
+  <footer className="bg-base-100 text-base-content p-4 shadow-inner mt-auto">
+    <p>
+      © RESTMates {new Date().getFullYear()} - All rights reserved by
+      RESTMates Industries Ltd
+    </p>
   </footer>
 );
 
---
+++ frontend/src/pages/ProfileEditPage.jsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from "react";
+import MemberProfileEdit from "../components/forms/MemberProfileEditForm";
+import { useAuth } from "../components/AuthProvider";
+import { useNavigate } from "react-router-dom";
+
+const fetchDeleteUser = async (userEmail) => {
+    try {
+      const response = await fetch(`/api/member/${userEmail}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return true;
+    } catch (error) {
+      console.error("Failed to delete user:", error);
+      return false;
+    }
+  };
+
+const fetchUserData = async (userEmail) => {
+  try {
+    const response = await fetch(`/api/member/${userEmail}`);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Failed to fetch member data:", error);
+    return null;
+  }
+};
+
+const fetchUpdateUser = async (userEmail, updatedUserData) => {
+    try {
+      const response = await fetch(`/api/member/${userEmail}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedUserData),
+      });
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error("Failed to update user data:", error);
+      return null;
+    }
+  };
+
+const ProfileEditPage = () => {
+  const [userData, setUserData] = useState(null);
+  const { userEmail, updateEmail, logout } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (userEmail) {
+      const loadMemberData = async () => {
+        const data = await fetchUserData(userEmail);
+        setUserData(data);
+      };
+      loadMemberData();
+    }
+  }, [userEmail]);
+
+  const handleUpdate = async (updatedUserData) => {
+    const data = await fetchUpdateUser(userEmail, updatedUserData);
+    setUserData(data);
+    if (updatedUserData.email !== userEmail) {
+        updateEmail(updatedUserData.email);
+      }
+    return data;
+  };
+  
+  const handleDelete = async () => {
+    const success = await fetchDeleteUser(userEmail);
+    if (success) {
+      logout();
+      navigate("/");
+    } else {
+      alert("Failed to delete profile");
+    }
+  };
+
+  return (
+    <div>
+      {userData ? (
+        <MemberProfileEdit memberData={userData}
+        onUpdate={handleUpdate}
+        onDelete={handleDelete} />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default ProfileEditPage;
