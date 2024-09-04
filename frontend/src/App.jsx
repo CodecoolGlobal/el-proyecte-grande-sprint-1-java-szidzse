28f90eb4a7a8e1db14b594f55c5cada30a4c6f9b
szidzse <gellertsos@gmail.com>
2024-09-04 14:04:24 +0200

    renames variables and props

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,29 +7,24 @@ import Footer from "./components/navigationBar/Footer.jsx";
 import ProfileViewPage from "./pages/ProfileViewPage.jsx";
 import ProfileEditPage from "./pages/ProfileEditPage.jsx";
 import AccommodationDetails from "./components/accommodations/AccommodationDetails.jsx";
-import CountrySearchInput from "./components/accommodations/SearchAccommodationInput.jsx";
 
 const App = () => {
-  return (
-    <div className="flex flex-col min-h-screen">
-      <NavigationBar />
-      <main className="flex-grow">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route
-            path="/accommodation/:accommodationId"
-            element={<AccommodationDetails />}
-          ></Route>
-          <Route path="/profile" element={<ProfileViewPage />} />
-          <Route path="/profileEdit" element={<ProfileEditPage />} />
-          <Route path="/search" element={<CountrySearchInput />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="flex flex-col min-h-screen">
+			<NavigationBar />
+			<main className="flex-grow">
+				<Routes>
+					<Route path="/" element={<HomePage />} />
+					<Route path="/register" element={<RegisterPage />} />
+					<Route path="/login" element={<LoginPage />} />
+					<Route path="/accommodation/:accommodationId" element={<AccommodationDetails />}></Route>
+					<Route path="/profile" element={<ProfileViewPage />} />
+					<Route path="/profileEdit" element={<ProfileEditPage />} />
+				</Routes>
+			</main>
+			<Footer />
+		</div>
+	);
 };
 
 export default App;
